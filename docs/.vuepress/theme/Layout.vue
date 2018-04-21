7567cc1992f0ff3dcd0e09de76dc89244790eeae
Andy Bell <me@andy-bell.co.uk>
2018-04-21 23:06:52 +0100

    Get codepen embeds working

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -14,7 +14,10 @@
             return {} 
         },
         mounted() {
-            
+            // Load in CodePen 
+            let codePenScript = document.createElement('script');
+            codePenScript.src = 'https://production-assets.codepen.io/assets/embed/ei.js';
+            document.head.appendChild(codePenScript);
         }
     }
 </script>
